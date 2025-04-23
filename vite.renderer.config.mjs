365774dcceff6a75e1cb9b15da85dafded34c387
Lockseed <liuqi2072@gmail.com>
2025-04-23 16:30:39 +0800

    fix type error

--- vite.renderer.config.mjs
+++ vite.renderer.config.mjs
@@ -2,18 +2,28 @@
 import { join } from 'node:path';
 import { defineConfig } from 'vite';
 
+
+/**
+ * @typedef {import('vite').ConfigEnv & { forgeConfigSelf: { name?: string } }} ForgeConfigEnv
+ */
+
 // https://vitejs.dev/config
-export default defineConfig((incomingConfigs) => {
-  const { mode, forgeConfigSelf } = incomingConfigs;
-  const name = forgeConfigSelf.name ?? "";
+export default defineConfig(
+  /**
+   * @param {ForgeConfigEnv} incomingConfigs
+   */
+  (incomingConfigs) => {
+    const { mode, forgeConfigSelf } = incomingConfigs;
+    const name = forgeConfigSelf.name ?? "";
 
-  const outDir = join(process.cwd(), `.vite/renderer/${name}`);
+    const outDir = join(process.cwd(), `.vite/renderer/${name}`);
 
-  return {
-    mode,
-    build: {
-      target: 'esnext',
-      outDir
+    return {
+      mode,
+      build: {
+        target: 'esnext',
+        outDir
+      }
     }
   }
-});
+);
